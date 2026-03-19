e32dbd066d2d667794f2efd6a5a00f29b2da6c27
vanessabastidas03 <Vanessa.bastidas@campusucc.edu.co>
2026-03-19 06:13:12 -0500

    feat: agrega imágenes a productos

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 const featuredProducts = [
-  { title: "Audífonos Bluetooth", price: 89900, tag: "OFERTA" },
-  { title: "Smartwatch Deportivo", price: 149900, tag: "TOP" },
-  { title: "Teclado Mecánico", price: 219900, tag: "NUEVO" },
-  { title: "Mouse Gamer", price: 79900, tag: "HOT" },
-  { title: "Laptop 14”", price: 1899900, tag: "ENVÍO" },
-  { title: "Silla Ergonómica", price: 499900, tag: "PRO" },
-  { title: "Cámara Web HD", price: 129900, tag: "CLASES" },
-  { title: "Parlante Portátil", price: 159900, tag: "MÚSICA" },
-  { title: "Tablet 10 pulgadas", price: 899900, tag: "NUEVO" },
+  { title: "Audífonos Bluetooth", price: 89900, tag: "OFERTA", image: "https://images.unsplash.com/photo-1518441902110-5bdc6f6d45c1" },
+  { title: "Smartwatch Deportivo", price: 149900, tag: "TOP", image: "https://images.unsplash.com/photo-1516574187841-cb9cc2ca948b" },
+  { title: "Teclado Mecánico", price: 219900, tag: "NUEVO", image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8" },
+  { title: "Mouse Gamer", price: 79900, tag: "HOT", image: "https://images.unsplash.com/photo-1587202372775-989d35c3c1e5" },
+  { title: "Laptop 14”", price: 1899900, tag: "ENVÍO", image: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8" },
+  { title: "Silla Ergonómica", price: 499900, tag: "PRO", image: "https://images.unsplash.com/photo-1582582494700-8c321c7e8b3f" },
+  { title: "Cámara Web HD", price: 129900, tag: "CLASES", image: "https://images.unsplash.com/photo-1587829741301-dc798b83add3" },
+  { title: "Parlante Portátil", price: 159900, tag: "MÚSICA", image: "https://images.unsplash.com/photo-1518444028785-8fbdbf6cd9f2" },
+  { title: "Tablet 10 pulgadas", price: 899900, tag: "NUEVO", image: "https://images.unsplash.com/photo-1542751110-97427bbecf20" }
 ];
-
 function formatCOP(value) {
   return new Intl.NumberFormat("es-CO", { style: "currency", currency: "COP" }).format(value);
 }
@@ -25,6 +24,7 @@ function renderProducts(targetEl, items) {
     const card = document.createElement("article");
     card.className = "card product";
     card.innerHTML = `
+      <img src="${p.image}" alt="${p.title}" class="product__img" />
       <h3 class="product__title">${p.title}</h3>
       <div class="product__meta">
         <span class="product__price">${formatCOP(p.price)}</span>
